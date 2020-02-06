b97b99b6597644a97068920d658b95fc84829441
CocoCouq <couq@icloud.com>
2020-02-06 22:26:01 +0100

    Front-End

---
+++ CSS - Materialize/Jarditou Materialize/assets/js/index.js
@@ -0,0 +1,12 @@
+// Nav bar -- Menu gauche
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
+
+// Tableau popout collapsible
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+  });
